c0f99e2e830c7aa064b978953cf07433ca4a4ddd
Karl Statz <karlstatz@karls-mbp.lan>
2018-03-18 20:08:47 -0500

    more scaffolding

---
+++ victorian/src/state_storage/state_channel_sup.erl
@@ -0,0 +1,18 @@
+
+-module(state_channel_sup).
+
+-define(SERVER, ?MODULE).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+%% API Definition
+start_link()->
+   supervisor:start_link({}, ?MODULE, []).
+
+%% Supervisor Callbacks
+init([]) ->
+    ok.
+    
+ 
